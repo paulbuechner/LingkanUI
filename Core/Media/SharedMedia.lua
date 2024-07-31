0e7870b76f1d3e2287332e3fe956d7e22133ccd9
Paul Büchner <paul-buechner@outlook.de>
2024-08-01 00:32:31 +0200

    feat: add custom `SharedMedia`

---
+++ Core/Media/SharedMedia.lua
@@ -0,0 +1,33 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+local MediaType_FONT = LSM.MediaType.FONT
+local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR
+local MediaType_BACKGROUND = LSM.MediaType.BACKGROUND
+local MediaType_SOUND = LSM.MediaType.SOUND
+
+-- -----
+-- BACKGROUND
+-- -----
+LSM:Register(MediaType_BACKGROUND, "Lingkan QuestIcon01", [[Interface\Addons\LingkanUI\Core\Media\Textures\QuestIcon01]])
+LSM:Register(MediaType_BACKGROUND, "Lingkan QuestIcon02", [[Interface\Addons\LingkanUI\Core\Media\Textures\QuestIcon02]])
+
+-- -----
+--  BORDER
+-- ----
+
+-- -----
+--   FONT
+-- -----
+LSM:Register(MediaType_FONT, "Avant", [[Interface\Addons\LingkanUI\Core\Media\Fonts\Avant.ttf]])
+LSM:Register(MediaType_FONT, "Big Noodle Tilting", [[Interface\Addons\LingkanUI\Core\Media\Fonts\Big Noodle Tilting.ttf]])
+LSM:Register(MediaType_FONT, "GothamNarrow-Bold", [[Interface\Addons\LingkanUI\Core\Media\Fonts\GothamNarrow-Bold.ttf]])
+LSM:Register(MediaType_FONT, "GothamNarrow-Medium", [[Interface\Addons\LingkanUI\Core\Media\Fonts\GothamNarrow-Medium.ttf]])
+LSM:Register(MediaType_FONT, "GothamNarrow-Ultra", [[Interface\Addons\LingkanUI\Core\Media\Fonts\GothamNarrow-Ultra.ttf]])
+
+-- -----
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
